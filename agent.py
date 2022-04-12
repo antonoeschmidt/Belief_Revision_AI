ebc39f8d9dce8c530eeffc0222ad476e86c9212d
Jens <jenswilliversen@gmail.com>
2022-04-12 09:39:41 +0200

    Agent shell created
    Co-Authored-By: siffravn <43875054+siffravn@users.noreply.github.com>
    Co-Authored-By: antonoeschmidt <55379109+antonoeschmidt@users.noreply.github.com>

---
+++ agent.py
@@ -0,0 +1,31 @@
+from knowledge_base import Knowledge_base
+
+
+class Agent():
+    def __init__(self,kb: Knowledge_base):
+        self.t = 0
+        self.kb = kb
+       
+
+    def __call__(self, percept):
+        self.tell(self.kb,self.make_percept_sentence(percept,self.t))
+        action = self.ask(self.kb,self.make_action_query(self.t))
+        self.tell(self.kb,self.make_action_Sentence(action,self.t))
+        self.t += 1
+        return action
+    
+    def tell(self,kb: Knowledge_base, percept_sentence):
+        
+        pass
+    
+    def ask(self, kb: Knowledge_base, action_query):
+        pass
+    
+    def make_percept_sentence(self):
+        pass
+
+    def make_action_query(self, t):
+        pass
+
+    def make_action_Sentence(self,action,t):
+        pass
---
+++ knowledge_base.py
@@ -0,0 +1,3 @@
+class Knowledge_base():
+    def __init__(self) -> None:
+        pass

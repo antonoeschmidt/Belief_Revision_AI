67e443ec9abfabd7cf2f7a70b0ed5bf992c02b07
Jenswill <61697954+Jenswill@users.noreply.github.com>
2022-04-26 11:01:46 +0200

    Update knowledge_base.py

--- knowledge_base.py
+++ knowledge_base.py
@@ -1,3 +1,11 @@
 class Knowledge_base():
-    def __init__(self) -> None:
-        pass
+    def __init__(self):
+        self.beliefs = []
+    
+    def add(self, belief):
+        self.beliefs.append(belief)
+
+
+    def print_self(self):
+        for belief in self.beliefs:
+                print(belief)
